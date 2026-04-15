ee0e158acc41a643ab7503380ee2c698d64b3d32
evan <evanlehner@gmail.com>
2026-04-14 19:47:10 -0700

    added stuff

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/evan/Documents/programming/CPP/cianGame
+# Install script for directory: /home/evan/Documents/programming/cpp/TextGame
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,7 +45,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/evan/Documents/programming/CPP/cianGame/build/install_local_manifest.txt"
+  file(WRITE "/home/evan/Documents/programming/cpp/TextGame/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/evan/Documents/programming/CPP/cianGame/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/evan/Documents/programming/cpp/TextGame/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,11 @@ public:
     };
 };
 
+struct StatusEffect {
+    std::string verb; // Burned, healed, died, etc.
+    std::function<void(Entity& e)> effect;
+};
+
 class Entity {
 public:
 
@@ -42,61 +47,188 @@ public:
     int health;
     Weapon* heldWeapon;
 
+    int statusTimer = 0; //If below zero, effect doesn't trigger
+    StatusEffect* statusEffect;
+
     void takeAction(Entity& to) {
 	Ability ability = heldWeapon->getRandomAbility();
 	ability.action(*this, to);
 	std::cout << name << " " << ability.verb << " with the " << heldWeapon->name << " (" << ability.effect << ").\n";
+
+	//status
+	if(statusTimer <= 0) return;
+	statusTimer--;
+	statusEffect->effect(*this);
+	std::cout << name << " " << statusEffect->verb << ".\n"; 
     };
 };
+
+
+struct Encounter {
+    Entity* enemy;
+    std::vector<Weapon*> weaponSelections;
+
+    Entity* generateEnemy() {
+	int weaponIndex = rand() % weaponSelections.size();
+	enemy->heldWeapon = weaponSelections[weaponIndex];
+	return enemy;
+    }
+};
+
 
 
 // ENTITY _ ENTITY with the _. (_)
 
 
 int main() {
-    Weapon stick{"twig"};
+
+    //GAME SETUP ---------
+    //Status Effects
+
+    StatusEffect onFire {"burned", [](Entity& e){ e.health -= 1; }};
+    StatusEffect cursed {"is cursed", [](Entity& e){ e.health -= 3; }};
+
+    //Weapons
+    Weapon debug{"debug"};
+    debug.abilityList = {
+	{"ERROR", "ERROR", [](Entity& from, Entity& to){ std::cout << "THIS SHOULD NOT HAPPEN! SOMETHING HAS GONE WRONG"; }}
+    };
+
+    Weapon stick{"Twig"};
     stick.abilityList = {
 	{"swung", "Dealt 1 damage", [](Entity& from, Entity& to){ to.health -= 1; }},
     };
 
-    Weapon sword{"Sword"};
-    sword.abilityList = {
-	{"Hit", "Dealth 5 damage",[](Entity& from, Entity& to){ to.health -= 5; }},
-	{"Hit", "Dealth 5 damage",[](Entity& from, Entity& to){ to.health -= 5; }},
-	{"Hit", "Dealth 5 damage",[](Entity& from, Entity& to){ to.health -= 5; }},
-	{"Hit", "Dealth 5 damage",[](Entity& from, Entity& to){ to.health -= 5; }},
-	{"Missed and hurt itself", "Dealt 2 damage", [](Entity& from, Entity& to){ from.health -= 2; }},
+    Weapon rustedSword{"Rusted Sword"};
+    rustedSword.abilityList = {
+	{"Swung", "Dealt 3 damage",[](Entity& from, Entity& to){ to.health -= 3; }},
+	{"Swung", "Dealt 3 damage",[](Entity& from, Entity& to){ to.health -= 3; }},
+	{"Swung", "Dealt 3 damage",[](Entity& from, Entity& to){ to.health -= 3; }},
+	{"Slipped and hurt itself", "Dealt 2 damage", [](Entity& from, Entity& to){ from.health -= 2; }},
 	{"Missed", "Did nothing", [](Entity& from, Entity& to){ }},
     };
 
-    Entity player {"Player", 100, &sword};
-    Entity basicEnemy {"Enemy", 10, &stick};
-    Entity strongEnemy {"Strong Enemy", 25, &sword};
+    Weapon massiveSword{"Massive Sword"};
+    massiveSword.abilityList = {
+	{"Swung", "Dealth 8 damage",[](Entity& from, Entity& to){ to.health -= 8; }},
+	{"Missed", "Did nothing",[](Entity& from, Entity& to){}},
+	{"Missed", "Did nothing",[](Entity& from, Entity& to){}},
+    };
+
+    Weapon dagger{"Dagger"};
+    dagger.abilityList = {
+	{"Stabbed", "Dealt 4 damage",[](Entity& from, Entity& to){ to.health -= 4; }},
+    };
+
+    Weapon flameDagger{"Fire Dagger"};
+    flameDagger.abilityList = {
+	{"Stabbed", "Dealt 2 damage",[&onFire](Entity& from, Entity& to){ to.health -= 2; to.statusEffect = &onFire; to.statusTimer = 2; }},
+	{"Stabbed", "Dealt 3 damage",[&onFire](Entity& from, Entity& to){ to.health -= 3; to.statusEffect = &onFire; to.statusTimer = 3; }},
+    };
 
 
-    Entity activeEnemy = basicEnemy;
 
+
+    //Entities
+    Entity player {"Player", 100, &rustedSword};
+    Entity basicEnemy {"Weak Enemy", 5, &debug};
+    Entity strongEnemy {"Strong Enemy", 15, &debug};
+
+    //Encounters
+    Encounter tutorialEncounter {&basicEnemy, {&flameDagger}};
+
+    Encounter basicEncounter {&basicEnemy, {&stick, &rustedSword}};
+    Encounter strongEncounter {&strongEnemy, {&rustedSword, &massiveSword, &dagger}};
+
+    std::vector<Encounter> encounterPool {basicEncounter, strongEncounter};
+
+    Entity activeEnemy = *tutorialEncounter.generateEnemy();
+
+
+    //GAME LOOP ----------
     bool isGameActive = true;
     while(isGameActive) {
 
 	if(activeEnemy.health <= 0) {
-	    std::cout << "The enemy has died.\n";
+	    std::cout << "The " << activeEnemy.name << " has died.\n";
+	    std::cout << "The path forward has three enemies.\n";
 	    activeEnemy = strongEnemy;
 	};
 
 	player.takeAction(activeEnemy);
-	activeEnemy.takeAction(player);
+	if(!(activeEnemy.health <= 0))
+	    activeEnemy.takeAction(player);
 	std::cout << "Player HP: " << player.health << " | Enemy HP: " << activeEnemy.health << "\n";
 
 	if(activeEnemy.health <= 0) {
 	    std::cout << "The enemy has died.\n";
-	    activeEnemy = strongEnemy;
+	    std::cout << "Would you like to steal it's weapon? (" << activeEnemy.heldWeapon->name << ")\n(y/n)";
+	    
+	    char c;
+	    bool accepted = false;
+	    while (!accepted) {
+		std::cin >> c;
+		switch(c) {
+		    case 'y':
+			std::cout << "You take the " << activeEnemy.heldWeapon->name << ".\n";
+			player.heldWeapon = activeEnemy.heldWeapon;
+			accepted = true;
+			break;
+		    case 'n':
+			accepted = true;
+			break;
+
+		    default:
+			std::cout << "Invalid character\n";
+		}
+	    }
+	   
+
+	    std::cout << "The path branches into three:\n";
+
+	    std::vector<Encounter> encounterChoices;
+
+	    for(int i = 1; i <= 3; i++) {
+		int encounterIndex = rand() % encounterPool.size();
+		Encounter e = encounterPool[encounterIndex];
+		encounterChoices.push_back(e);
+
+		std::cout << i << ": A " << e.enemy->name << ".\n";
+	    };
+
+	    std::cout << "Who would you like to fight? (1-3): ";
+	    
+	    accepted = false;
+	    while (!accepted) {
+		std::cin >> c;
+
+		switch(c) {
+		    case '1': 
+			std::cout << "You take the left path.\n";
+			activeEnemy = *(encounterChoices[0].generateEnemy());
+			accepted = true;
+			break;
+
+		    case '2': 
+			std::cout << "You take the middle path.\n";
+			activeEnemy = *(encounterChoices[1].generateEnemy());
+			accepted = true;
+			break;
+
+		    case '3': 
+			std::cout << "You take the right path.\n";
+			activeEnemy = *(encounterChoices[2].generateEnemy());
+			accepted = true;
+			break;
+
+		    default:
+			std::cout << "Invalid choice\n";
+		}
+	    };
+
+	    std::cin.get();
+	    std::cout << "A " << activeEnemy.name << " blocks your path wielding a " << activeEnemy.heldWeapon->name << "!\n";
 	};
-
-
-
-
-
 
 	std::cin.get();
     };
